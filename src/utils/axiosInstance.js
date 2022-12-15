506e8b49bbd8459422c19debc71581d885123350
MarceloVBrasil <marcelo.vital.brasil@gmail.com>
2022-12-15 02:13:18 -0300

    add SERVER_URL env variable

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8060",
+  baseURL: process.env.SERVER_URL,
   headers: {
     "Content-Type": "application/json",
   },
