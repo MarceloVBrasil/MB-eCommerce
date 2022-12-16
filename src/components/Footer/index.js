fc07ec8f80e4b7eb147a129c4fa467e4e30e0955
MarceloVBrasil <marcelo.vital.brasil@gmail.com>
2022-12-16 11:41:09 -0300

    link to my website

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ import "./Footer.scss"
 export default function Footer() {
   return (
       <footer className='footer'>
-          &copy; Marcelo Brasil | fake e-Commerce website | <a href="mailto:marcelo.vital.brasil@gmail.com"> Contact Me</a>
+         <a href="https://marcelobrasil.ca"> &copy; Marcelo Brasil </a> | fake e-Commerce website | <a href="mailto:marcelo.vital.brasil@gmail.com"> Contact Me</a>
       </footer>
   )
 }
