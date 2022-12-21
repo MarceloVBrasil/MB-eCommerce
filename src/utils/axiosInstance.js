62efd4d86bf3f63e5ca21073102099b8750fb21a
MarceloVBrasil <marcelo.vital.brasil@gmail.com>
2022-12-21 19:40:22 -0300

    change url

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_URL,
-    //"https://mbecommerce.herokuapp.com",
+  baseURL: //process.env.REACT_APP_SERVER_URL,
+    "https://mbecommerce.herokuapp.com",
   headers: {
     "Content-Type": "application/json",
   },
